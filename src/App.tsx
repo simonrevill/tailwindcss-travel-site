241e66a9a81293ea0d43e5b7a2062d47f471ef43
Simon Revill <34922287+simonrevill@users.noreply.github.com>
2023-09-25 20:43:08 +0100

    adds mobile responsive header and nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { MdOutlineHelpOutline } from "react-icons/md";
+import { MdMenu, MdClose, MdOutlineHelpOutline } from "react-icons/md";
+import { BiUser } from "react-icons/bi";
 import { gb } from "./assets/images";
 import { navItems } from "./constants";
 
 const App = () => {
   const [activeNavItem, setActiveNavItem] = useState("Stays");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(true);
 
   return (
     <>
@@ -14,38 +16,68 @@ const App = () => {
             <a href="/" className="text-xl font-bold text-white">
               Booking.com
             </a>
-            <div className="flex items-center gap-8">
-              <a href="/" className="text-sm font-bold text-white">
-                GBP
-              </a>
-              <a href="/">
-                <img
-                  src={gb}
-                  alt="Language - English"
-                  className="rounded-full"
-                  width={24}
-                  height={24}
-                />
-              </a>
-              <a href="/" className="text-white">
-                <MdOutlineHelpOutline size="1.5rem" />
-              </a>
-              <a href="/" className="text-sm font-bold text-white">
-                List your property
-              </a>
+            <div className="relative flex items-center gap-8">
+              <div
+                className={`items-center gap-8 max-lg:absolute max-lg:right-0 max-lg:top-[1.75rem] max-lg:w-[50vw] max-lg:border-2 max-lg:border-blue-500 max-lg:bg-white max-lg:p-4 lg:flex ${
+                  isMobileMenuOpen
+                    ? "flex max-lg:flex-col max-lg:items-start"
+                    : "hidden"
+                }`}
+              >
+                <a
+                  href="/"
+                  className="text-sm font-bold text-white max-lg:text-black"
+                >
+                  GBP
+                </a>
+                <a href="/">
+                  <img
+                    src={gb}
+                    alt="Language - English"
+                    className="rounded-full"
+                    width={24}
+                    height={24}
+                  />
+                </a>
+                <a href="/" className="text-white max-lg:text-black">
+                  <MdOutlineHelpOutline size="1.5rem" />
+                </a>
+                <a
+                  href="/"
+                  className="text-sm font-bold text-white max-lg:text-black"
+                >
+                  List your property
+                </a>
+              </div>
               <div className="flex gap-2">
+                <a
+                  href="/"
+                  className="rounded-full border p-1 text-white md:hidden"
+                >
+                  <BiUser size="1.25rem" />
+                </a>
                 <button
                   type="button"
-                  className="min-w-[4.375rem] rounded-sm border border-blue-400 bg-white p-1.5 text-sm font-semibold text-blue-500 hover:bg-blue-50"
+                  className="hidden min-w-[4.375rem] rounded-sm border border-blue-400 bg-white p-1.5 text-sm font-semibold text-blue-500 hover:bg-blue-50 md:block"
                 >
                   Register
                 </button>
                 <button
                   type="button"
-                  className="min-w-[4.375rem] rounded-sm border border-blue-400 bg-white p-1.5 text-sm font-semibold text-blue-500 hover:bg-blue-50"
+                  className="hidden min-w-[4.375rem] rounded-sm border border-blue-400 bg-white p-1.5 text-sm font-semibold text-blue-500 hover:bg-blue-50 md:block"
                 >
                   Sign in
                 </button>
+              </div>
+              <div
+                className="cursor-pointer text-white lg:hidden"
+                onClick={() => setIsMobileMenuOpen((prevState) => !prevState)}
+              >
+                {isMobileMenuOpen ? (
+                  <MdClose size="1.5rem" />
+                ) : (
+                  <MdMenu size="1.5rem" />
+                )}
               </div>
             </div>
           </div>
